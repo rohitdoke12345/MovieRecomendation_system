6f331d7103c9591a0fea49f4dfe72d3df6eb3442
rohitdok <rohitdok09@gmail.com>
2024-06-14 18:10:07 +0530

    small change give text message

--- src/MRSRepository/RatingReviewRepository.java
+++ src/MRSRepository/RatingReviewRepository.java
@@ -74,4 +74,7 @@ public class RatingReviewRepository extends DBHelper {
 
 	}
 
+	
+
+
 }
--- src/MRSService/RatingReviewService.java
+++ src/MRSService/RatingReviewService.java
@@ -18,5 +18,6 @@ public class RatingReviewService {
 			
 			return rrrepo.GetOverallRating();
 		}
+
         
 }
